61ac5b07ef9ca9c6bc75fe996b89a342946501c2
AudriusAnusauskas <82049110+AudriusAnusauskas@users.noreply.github.com>
2022-04-02 21:49:06 +0300

    test 1

--- test.js
+++ test.js
@@ -1,20 +1,43 @@
-let born = 0;
-let dead = 0
-const bornDead = function (a, b){
+// let born = document.getElementById('born');
+// let dead = document.getElementById('dead');
+
+let bornCount =0;
+let deadCount=0;
+
+const bornDead = setInterval(function (a, b){
+    a = 4.4;
+    b=1.8;
     for(let i=0; i<=a; i++){
-        born += Math.floor(a);
+        bornCount += Math.floor(a);
+        born = document.getElementById('born');
+        born.innerHTML;
     }
     for(let j=0; j<=b; j++){
-        dead += Math.floor(b)
+        deadCount += Math.floor(b);
+        dead = document.getElementById('dead');
+        
     }
 
     // return (born, dead)
     console.log(born, dead);
-}
-
-bornDead(4.4, 1.8)
+}, 1000)
 
 
+
+// setInterval(() => {
+//     for(let i=0; i<=4.4; i++){
+//         born += Math.floor(4.4);
+//     }
+//     born.innerHTML;
+
+//     for(let j=0; j<=1.8; j++){
+//         dead += Math.floor(1.8)
+//     }
+//     dead.innerHTML;
+
+// bornDead(4.4, 1.8)
+
+// })
 
 // let born = 0;
 // setInterval(function (a = 4) {born += a}, 1000 );
