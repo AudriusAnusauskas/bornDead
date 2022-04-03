1456b31341d24fe7f1ffe5d35397e04ee46bf273
AudriusAnusauskas <82049110+AudriusAnusauskas@users.noreply.github.com>
2022-04-03 16:21:42 +0300

    counter works

--- test.js
+++ test.js
@@ -1,17 +1,27 @@
 let bornCount =0;
 let deadCount=0;
 
-setInterval(function (a, b){
-    a = 4.4;
-    b=1.8;
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', function handleClick(event) {
+
+event.preventDefault();
+
+let a = Number(document.getElementById('avgBorn').value);
+let b = Number(document.getElementById('avgDead').value);
+
+function bornDead(){
+            
+        let roundedBornCount = Math.round(bornCount += a);
+        document.getElementById('born').innerHTML = roundedBornCount;
     
-    let roundedBornCount = Math.round(bornCount += a);
-    document.getElementById('born').innerHTML = roundedBornCount;
+        let roundedDeadCount = Math.round(deadCount += b);
+        document.getElementById('dead').innerHTML = roundedDeadCount;
+            
+        console.log(roundedBornCount, roundedDeadCount);
+        return false
+    }
+    
+    console.log(setInterval(bornDead, 1000));
 
-    let roundedDeadCount = Math.round(deadCount += b);
-    document.getElementById('dead').innerHTML = roundedDeadCount;
-        
-    console.log(roundedBornCount, roundedDeadCount);
-}, 1000)
-
-
+})
