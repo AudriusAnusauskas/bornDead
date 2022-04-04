a4fe0f112ad90c78eeaa7879d5174cfbeca75543
AudriusAnusauskas <82049110+AudriusAnusauskas@users.noreply.github.com>
2022-04-04 18:27:15 +0300

    var 1

--- test.js
+++ test.js
@@ -19,12 +19,32 @@ function bornDead(){
         let roundedDeadCount = Math.round(deadCount += b);
         document.getElementById('dead').innerHTML = roundedDeadCount;
             
+        if (a > 0 && a < 100) {
+          let targetDiv = document.getElementById('innerdivs')
+          // let sheet = document.createElement('style')
+          // sheet.innerHTML = "div {flex: ; border: 2px solid black; width: 20px; height: 10%; background-color: blue;}";
+          // targetDiv.appendChild(sheet);
+          var tempDiv = document.createElement('div');
+          tempDiv.style.display = 'inline';
+          tempDiv.style.backgroundColor = 'pink';
+          // tempDiv.style.height = '20px';
+          // tempDiv.style.width = '20px';
+          tempDiv.style.margin = '2px';
+          tempDiv.style.border = 'solid black 1px';
+
+          
+          if (targetDiv) {
+            targetDiv.insertAdjacentElement('afterend', tempDiv);
+  }
+        }
+        
         console.log(roundedBornCount, roundedDeadCount);
         return false
-    }
+      }
+      
+      setInterval(bornDead, 1000);
+      
+    }, {once : true});
     
-  setInterval(bornDead, 1000);
-
-})
-
-btn2.addEventListener('click', clearInterval())
+    btn2.addEventListener('click', clearInterval())
+    
