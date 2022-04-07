65b8003a207d41a6b72bcad942376b81fd911e01
AudriusAnusauskas <82049110+AudriusAnusauskas@users.noreply.github.com>
2022-04-07 18:53:56 +0300

    img el

--- test.js
+++ test.js
@@ -24,10 +24,12 @@ function bornDead(){
           // let sheet = document.createElement('style')
           // sheet.innerHTML = "div {flex: ; border: 2px solid black; width: 20px; height: 10%; background-color: blue;}";
           // targetDiv.appendChild(sheet);
-          var tempDiv = document.createElement('span');
+          var tempDiv = document.createElement('img');
           tempDiv.setAttribute(
             'style',
-            'background-color: salmon; color: white; width: 15px; height: 15px; border: solid black 1px;',
+            'display: inline; color: white; width: 15px; height: 15px; border: solid black 1px;',
+            'src',
+            './img/images.jpeg'
           );
           
           if (targetDiv) {
